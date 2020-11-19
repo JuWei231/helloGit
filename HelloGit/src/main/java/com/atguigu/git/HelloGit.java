489dbcf5242e7f0cf23354faf837988c892790c8
JuWei231 <492647591@qq.com>
2020-11-19 12:31:32 +0800

    更新1

--- HelloGit/src/main/java/com/atguigu/git/HelloGit.java
+++ HelloGit/src/main/java/com/atguigu/git/HelloGit.java
@@ -2,6 +2,7 @@ package com.atguigu.git;
 
 public class HelloGit {
     public static void main(String[] args) {
-        System.out.println();
+        System.out.println("HelloWorld");
+        System.out.println("更新1");
     }
 }
